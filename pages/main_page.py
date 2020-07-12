4a2efd2b5c072312c23d71135e91785ea8b91ceb
Zoya Kostyreva <zoya.kostyreva@gmail.com>
2020-07-12 13:56:53 +0500

    Add link navigation method

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,6 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
 
-
 class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
